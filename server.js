440517a7cfdf7468c787aeb4c90f72667f38806e
Josh Gragg <69860217+Joshg7777@users.noreply.github.com>
2022-09-01 00:17:01 -0400

    A01

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const server = http.createServer((req, res) => {
 })
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port, hostname => { 
+server.listen(port, hostname, () => { 
     console.log('Server listening on port ${port}')
 })
 
